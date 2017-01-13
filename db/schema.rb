971d9507f70c07935302c350026ab993b8cf10ca
Learn <learn@learn11.local>
2017-01-13 11:33:47 -0800

    Added correct db file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112213637) do
+ActiveRecord::Schema.define(version: 20170113193048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
